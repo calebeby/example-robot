ffb508b033b720bb1fe817a1a29c9384c7d64d63
Caleb Eby <caleb.eby01@gmail.com>
2020-07-15 19:28:03 -0700

    Add sensor to ball intake

--- src/main/java/frc/robot/subsystems/BallIntake.java
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BallIntake extends SubsystemBase {
     private SpeedController intakeMotor = new VictorSP(5);
+    private AnalogInput ballSensor = new AnalogInput(0);
 
     public void intake() {
         intakeMotor.set(1);
@@ -16,6 +18,14 @@ public class BallIntake extends SubsystemBase {
     }
 
     public void stop() {
-        intakeMotor.set(0);
+        if (hasBall()) {
+            intakeMotor.set(0.1);
+        } else {
+            intakeMotor.set(0);
+        }
+    }
+
+    public boolean hasBall() {
+        return ballSensor.getVoltage() < 2.0;
     }
 }
