5b399477fb1b4c25b24108ded654bf47043ff1e5
Caleb Eby <caleb.eby01@gmail.com>
2020-07-22 19:07:38 -0700

    Remove unused field

--- src/main/java/frc/robot/commands/JoystickElevatorCommand.java
+++ src/main/java/frc/robot/commands/JoystickElevatorCommand.java
@@ -2,13 +2,11 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Elevator;
 
 public class JoystickElevatorCommand extends CommandBase {
     private Elevator elevator;
-	private Joystick joystick;
     private DoubleSupplier speedSupplier;
 
     public JoystickElevatorCommand(Elevator elevator, DoubleSupplier speedSupplier) {
